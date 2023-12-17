94bd3e60cf85e1d5c59c7e210cfbe3fd84138253
root <root@ip-172-31-19-81.us-west-2.compute.internal>
2023-12-17 13:24:50 +0000

    1-ch

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,9 +2,10 @@ def call(Map stageParams) {
 
     checkout([
         $class: 'GitSCM',
-        branches: [(name: stageParams.branch )],
-        userRemoteConfigs: [( url: stageParams.url )]
+        branches: [[name: stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-} 
+}
+
 
 
